e48afe78219c26fd121eee30fed6e0fafc61aaba
say8hi <maks.mir@yahoo.com>
2024-03-09 13:07:20 +0500

    fix

--- internal/middlewares/auth.go
+++ internal/middlewares/auth.go
@@ -6,29 +6,28 @@ import (
 	"strings"
 
 	"github.com/say8hi/go-api-test/internal/database"
+	"github.com/say8hi/go-api-test/internal/utils"
 )
-
-
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-  receivedHash := r.Header.Get("Authorization")
-  if !strings.HasPrefix(receivedHash, "Bearer ") {
-    http.Error(w, "Bad Authorization header format.", http.StatusBadRequest)
-    return 
-  }
+		receivedHash := r.Header.Get("Authorization")
+		if !strings.HasPrefix(receivedHash, "Bearer ") {
+			utils.SendJSONError(w, "Bad Authorization header format.", http.StatusBadRequest)
+			return
+		}
 
-  receivedHash = strings.TrimPrefix(receivedHash, "Bearer ")
-  _, err := database.GetUserByPasswordHash(receivedHash)
-  if err == sql.ErrNoRows { 
-      http.Error(w, "Unauthorized", http.StatusConflict)
-      return
-  } else if err != nil{ 
-      http.Error(w, "Database error", http.StatusInternalServerError)
-      return
-  }
+		receivedHash = strings.TrimPrefix(receivedHash, "Bearer ")
+		_, err := database.GetUserByPasswordHash(receivedHash)
+		if err == sql.ErrNoRows {
+			utils.SendJSONError(w, "Unauthorized", http.StatusConflict)
+			return
+		} else if err != nil {
+			utils.SendJSONError(w, "Database error", http.StatusInternalServerError)
+			return
+		}
 
-	next.ServeHTTP(w, r)
+		next.ServeHTTP(w, r)
 	})
 }
